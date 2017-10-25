0b433c979d35bee40b8d5eb32d0899cfdd2b4a38
Brooken2 <nel13012@byui.edu>
2017-10-25 14:37:11 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/evans/MainActivity.java
+++ app/src/main/java/com/example/evans/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.evans;
+    package com.example.evans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+   /** 
+   * We can now have a merge Conflic
+   */
 
     /**
      * Danny: test comment
