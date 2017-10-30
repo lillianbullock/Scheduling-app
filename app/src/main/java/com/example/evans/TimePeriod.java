c9e9633e15d527bc384bdd8adccf8407fc3a631e
L. Brooke Bullock <lillianbullock@gmail.com>
2017-10-30 14:28:08 -0600

    Created Time Period Enum

---
+++ app/src/main/java/com/example/evans/TimePeriod.java
@@ -0,0 +1,14 @@
+package com.example.evans;
+
+/**
+ * Created by brooke on 10/27/17.
+ * Enum to make categorization of the goals and other things
+ * with time periods attactched easier
+ */
+
+public enum TimePeriod {
+    Day,
+    Week,
+    Month,
+    Year
+}
