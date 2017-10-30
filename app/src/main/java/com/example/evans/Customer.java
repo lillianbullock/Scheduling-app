fe2fcecf640462e7091638d2f012dc6e8dc96130
Brooke <nel13012@byui.edu>
2017-10-30 14:46:37 -0600

    Adding the Customer Class

---
+++ app/src/main/java/com/example/evans/Customer.java
@@ -0,0 +1,27 @@
+package com.example.evans;
+
+import java.util.List;
+
+/**
+ * Created by Brooke Nelson on 10/30/2017.
+ * This is the Customer class:
+ *  It will handle and work with the customer information
+ *
+ */
+
+public class Customer {
+    String _name;
+    String _email;
+    String _phone;
+
+    //CHANGE TO APPOINTMENT LIST WHEN WE DO APPOINTMENT CLASS
+    List<String> _appointmentsList;
+
+    Customer(){
+        _name = "";
+        _email = "";
+        _phone = "";
+    }
+
+
+}
