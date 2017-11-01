1c1f0701d310305c4b92d05dadfa34dfbe636339
Brooke <nel13012@byui.edu>
2017-10-31 23:28:19 -0600

    Adding a Appointment Test (had to add the class so i could start my test

---
+++ app/src/main/java/com/example/evans/Appointment.java
@@ -0,0 +1,8 @@
+package com.example.evans;
+
+/**
+ * Created by Brooke Nelson on 10/31/2017.
+ */
+
+class Appointment {
+}
---
+++ app/src/test/java/com/example/evans/appointmentTest.java
@@ -0,0 +1,16 @@
+package com.example.evans;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Brooke Nelson on 10/31/2017.
+ */
+
+public class appointmentTest {
+    @Test
+    public void doesSomething() throws Exception {
+    }
+
+}
