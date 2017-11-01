9610b8b7c9afd21174b4f68a4ed37dc8796c7784
Brooke <nel13012@byui.edu>
2017-10-31 19:55:23 -0600

    Adding GET and Setters

--- app/src/main/java/com/example/evans/Expense.java
+++ app/src/main/java/com/example/evans/Expense.java
@@ -16,4 +16,19 @@ public class Expense {
         _name = "";
         _price = 0.0;
     }
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String name) {
+        this._name = name;
+    }
+
+    public Double getPrice() {
+        return _price;
+    }
+    public void setPrice(Double price) {
+        this._price = price;
+    }
 }
