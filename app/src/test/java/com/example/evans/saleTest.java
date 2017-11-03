833187edb99ff1457375331d1fa969a4d3371ac7
Brooke <nel13012@byui.edu>
2017-11-02 21:52:08 -0600

    Sales Test

--- app/src/test/java/com/example/evans/saleTest.java
+++ app/src/test/java/com/example/evans/saleTest.java
@@ -1,8 +1,37 @@
 package com.example.evans;
 
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 /**
  * Created by Brooke Nelson on 11/2/2017.
  */
 
 public class saleTest {
+    @Test
+    public void testCons() throws Exception{
+        Service service = new Service();
+        Double price = 12.99;
+        LocalDateTime dateTime = LocalDateTime.now();
+        Customer customer = new Customer();
+        Boolean isByAppointment = true;
+        Boolean isByCustomer = true;
+
+        Sale sale = new Sale(service, price, dateTime, customer,
+                isByAppointment, isByCustomer);
+
+        //Just in case i feel like we need to check the Default to make sure it doesn't have weird values.
+        Sale sale1 = new Sale();
+        assertNull(sale1);
+
+        //First we must check to see if the Appointment itself was not created correctly
+        assertNotNull(sale);
+
+        //assertEquals(12.99, sale.getPrice());
+    }
 }
