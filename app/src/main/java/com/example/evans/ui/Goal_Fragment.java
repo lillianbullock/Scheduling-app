8da0cbc61dd385f33731f4ca8d7ff83acf6f38ff
Brooke <nel13012@byui.edu>
2017-11-08 23:23:55 -0700

    Goal Fragment look done

--- app/src/main/java/com/example/evans/ui/Goal_Fragment.java
+++ app/src/main/java/com/example/evans/ui/Goal_Fragment.java
@@ -1,4 +1,4 @@
-package com.example.evans;
+package com.example.evans.ui;
 
 
 import android.os.Bundle;
@@ -7,14 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.evans.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
+ *
  */
 public class Goal_Fragment extends Fragment {
 
 
     public Goal_Fragment() {
+        //Goal Fragment
         // Required empty public constructor
     }
 
