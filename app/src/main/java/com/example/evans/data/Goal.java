660537ac84a13669f3531c3939dddbaebc610234
Brooke <nel13012@byui.edu>
2017-12-09 16:05:02 -0700

    Repeat time taken out

--- app/src/main/java/com/example/evans/data/Goal.java
+++ app/src/main/java/com/example/evans/data/Goal.java
@@ -20,8 +20,6 @@ public class Goal implements Comparable {
     private String _description;
     private LocalDate _dueDate;
     private LocalDate _startDate;
-    private Boolean _repeat;
-    private TimePeriod _repeatCycle;
     private Boolean _done;
 
     public Goal() {
@@ -30,19 +28,7 @@ public class Goal implements Comparable {
         _description = "";
         _dueDate = LocalDate.now();
         _startDate = LocalDate.now();
-        setRepeatCycle(null);
         _done = null;
-    }
-
-    public Goal(String title, String description, LocalDate dueDate,
-         LocalDate startDate, TimePeriod repeatCycle) {
-        this._id = "";
-        this._title = title;
-        this._description = description;
-        this._dueDate = dueDate;
-        this._startDate = startDate;
-        this._done = false;
-        setRepeatCycle(repeatCycle);
     }
 
     public Goal(String title, String description, LocalDate dueDate,
@@ -53,8 +39,6 @@ public class Goal implements Comparable {
         this._dueDate = dueDate;
         this._startDate = startDate;
         this._done = false;
-        _repeatCycle = null;
-        _repeat = false;
     }
 
 
@@ -95,18 +79,6 @@ public class Goal implements Comparable {
     public String getDueDate() { return _dueDate.toString(); }
     public void setDueDate(String dueDateString) { this._dueDate = LocalDate.parse(dueDateString); }
 
-    public Boolean isRepeat() { return _repeat; }
-    public TimePeriod getRepeatCycle() { return _repeatCycle; }
-    /**
-     * sets the repeat cycle to the passed value and sets _isRepeat based on that
-     * @param repeatCycle: TimePeriod that is how often the goal repeats
-     */
-    public void setRepeatCycle(TimePeriod repeatCycle) {
-        _repeatCycle = repeatCycle;
-        if (_repeatCycle == null) {
-            _repeat = false;
-        } else _repeat = true;
-    }
 
     public Boolean isDone() { return _done; }
     public void setDone(Boolean done) {this._done = done; }
