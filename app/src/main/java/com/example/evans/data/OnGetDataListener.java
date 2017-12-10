02b5a149fdf80b724c902efcc9fa66a1fb21503a
Daniel Ekpo <daniekpo@gmail.com>
2017-12-09 21:46:26 -0700

    Added javaDoc comments to FirebaseManager

--- app/src/main/java/com/example/evans/data/OnGetDataListener.java
+++ app/src/main/java/com/example/evans/data/OnGetDataListener.java
@@ -4,12 +4,12 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 
 /**
- * This is an interface to implement when making a firebase call since
+ * Interface to implement when making a firebase call since
  * it runs asynchronously
  */
 
 public interface OnGetDataListener {
-    //interface functions are public
+
     void onDataLoadStarted();
     void onDataLoadSucceed(DataSnapshot dataSnapshot);
     void onDataLoadFailed(DatabaseError databaseError);
