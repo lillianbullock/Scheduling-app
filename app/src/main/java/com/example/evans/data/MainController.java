ed585bf29a9fa240189f9e81d42dd3993f3ee146
L. Brooke Bullock <lillianbullock@gmail.com>
2017-12-09 21:57:46 -0700

    javadocs in MainController

--- app/src/main/java/com/example/evans/data/MainController.java
+++ app/src/main/java/com/example/evans/data/MainController.java
@@ -210,36 +210,53 @@ public class MainController {
         });
     }
 
-
-
+    /**
+     * queries database for the expenses
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getAllExpenses(OnGetDataListener onGetDataListener) {
         _firebaseManager.getAllExpenses(onGetDataListener);
     }
 
+    /**
+     * queries database for the customers
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getAllCustomers(OnGetDataListener onGetDataListener) {
         _firebaseManager.getAllCustomers(onGetDataListener);
     }
 
+    /**
+     * queries database for the appointments
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getAllAppointments(OnGetDataListener onGetDataListener) {
         _firebaseManager.getAllAppointments(onGetDataListener);
     }
 
+    /**
+     * queries database for the goals
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getAllGoals(OnGetDataListener onGetDataListener){
         _firebaseManager.getAllGoals(onGetDataListener);
     }
 
+    /**
+     * queries database for the sales
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getAllService(OnGetDataListener onGetDataListener){
       //  _firebaseManager.getAllServices(onGetDataListener);
     }
-
 
     /**
      * Add a customer to the list of customers
      * This method will also set the customer's id, and
      * return the edited customer
      * No validation is carried out in this function
-     * @param customer: valid customer to be added to the list
-     * @return : passed customer with set id value
+     * @param customer valid customer to be added to the list
+     * @return passed customer with set id value
      */
     public Customer addCustomer(Customer customer) {
         if (customer == null) {
@@ -263,7 +280,7 @@ public class MainController {
     /**
      * AddSale: Add a sale to the list of sales.
      * No validation is carried out in this function
-     * @param sale: The sale to be added to the sales list
+     * @param sale The sale to be added to the sales list
      */
     public void addSale(Sale sale) {
 
@@ -282,7 +299,7 @@ public class MainController {
     /**
      * Add a single goal to the list of goals.
      * No validation is carried out in this function
-     * @param goal: The goal to be added
+     * @param goal The goal to be added
      */
     public void addNewGoal(Goal goal) {
 
@@ -301,7 +318,7 @@ public class MainController {
     /**
      * Add a valid expense to the list of expenses. Again the list item here should be in
      * a valid state.
-     * @param expense: expense to be added to the list.
+     * @param expense expense to be added to the list.
      */
     public void addExpense(Expense expense) {
 
@@ -317,7 +334,7 @@ public class MainController {
     /**
      * Add an appointment to the list of appointments.
      * No validation is carried out in this function
-     * @param appointment: valid appointment to be added to the list
+     * @param appointment valid appointment to be added to the list
      */
     public void addAppointment(Appointment appointment) {
         if (appointment == null) {
@@ -331,6 +348,8 @@ public class MainController {
 
     /**
      * Add a new service to the list of services that is offered
+     * @param title title of the service
+     * @param service the service to be added
      */
     public void addService(String title, Service service){
 
@@ -345,6 +364,7 @@ public class MainController {
 
     /**
      * Add a new service to the list of services that is offered
+     * @param service service to be added
      */
     public void addService(Service service){
 
@@ -380,6 +400,12 @@ public class MainController {
         return newCustomer;
     }
 
+    /**
+     * Update a appointment information in the database and in the session's copy
+     * @param oldAppointment the old appointment to be replaced
+     * @param newAppointment new appointment
+     * @return the new appointment with the ID set
+     */
     public Appointment updateAppointment(Appointment oldAppointment, Appointment newAppointment) {
 
         int oldAppointmentIndex = _appointments.indexOf(oldAppointment);
@@ -395,6 +421,11 @@ public class MainController {
         return newAppointment;
     }
 
+    /**
+     * todo nelson can comment on this one
+     * @param newAppointment
+     * @return
+     */
     public Appointment updateAppointment(Appointment newAppointment) {
 
         if (newAppointment == null) {
@@ -410,6 +441,12 @@ public class MainController {
         return newAppointment;
     }
 
+    /**
+     * Update a sale information in the database and in the session's copy
+     * @param oldSale sale to be replaced
+     * @param newSale new sale
+     * @return updated sale with id
+     */
     public Sale updateSale(Sale oldSale, Sale newSale) {
 
         int oldSaleIndex = _allSales.indexOf(oldSale);
@@ -426,6 +463,12 @@ public class MainController {
 
     }
 
+    /**
+     * Update a goal information in the database and in the session's copy
+     * @param oldGoal goal to be replaced
+     * @param newGoal new goal
+     * @return the updated goal with the id
+     */
     public Goal updateGoal(Goal oldGoal, Goal newGoal) {
         int oldGoalIndex = _goals.indexOf(oldGoal);
 
@@ -440,6 +483,12 @@ public class MainController {
         return newGoal;
     }
 
+    /**
+     * Update a customer information in the database and in the session's copy
+     * @param oldService service to be updated
+     * @param newService new service
+     * @return updated service with id
+     */
     public Service updateService(Service oldService, Service newService) {
 
         int oldServiceIndex = _services.indexOf(oldService);
@@ -455,6 +504,12 @@ public class MainController {
         return newService;
     }
 
+    /**
+     * Update a expense information in the database and in the session's copy
+     * @param oldExpense expense to be replaced
+     * @param newExpense new expense
+     * @return updated expense with id
+     */
     public Expense updateExpense(Expense oldExpense, Expense newExpense) {
 
         int oldExpenseIndex = _expenses.indexOf(oldExpense);
@@ -470,8 +525,11 @@ public class MainController {
         return newExpense;
     }
 
-
-
+    /**
+     * deletes customer from firebase
+     * @param customer to be deleted
+     * @return success
+     */
     public boolean deleteCustomer(Customer customer){
 
         if (customer == null || _customers.indexOf(customer) < 0) {
@@ -486,6 +544,11 @@ public class MainController {
         return _customers.remove(customer);
     }
 
+    /**
+     * deletes appointment from firebase
+     * @param appointment to be deleted
+     * @return success
+     */
     public boolean deleteAppointment(Appointment appointment){
 
         if (appointment == null || _appointments.indexOf(appointment) < 0) {
@@ -500,6 +563,11 @@ public class MainController {
         return _appointments.remove(appointment);
     }
 
+    /**
+     * deletes goal from firebase
+     * @param goal to be deleted
+     * @return success
+     */
     public boolean deleteGoal(Goal goal){
 
         if (goal == null || _goals.indexOf(goal) < 0) {
@@ -514,7 +582,11 @@ public class MainController {
         return _goals.remove(goal);
     }
 
-
+    /**
+     * deletes service from firebase
+     * @param service to be deleted
+     * @return success
+     */
     public boolean deleteService(Service service) {
         if (service == null || _services.indexOf(service) < 0) {
             // the service doesn't exist
@@ -528,6 +600,11 @@ public class MainController {
         return _services.remove(service);
     }
 
+    /**
+     * deletes sale from firebase
+     * @param sale to be deleted
+     * @return success
+     */
     public boolean deleteSale(Sale sale){
         if (sale == null || _allSales.indexOf(sale) < 0) {
             // the sale doesn't exist
@@ -541,6 +618,11 @@ public class MainController {
         return _allSales.remove(sale);
     }
 
+    /**
+     * deletes expense from firebase
+     * @param expense to be deleted
+     * @return success
+     */
     public boolean deleteExpense(Expense expense){
         if (expense == null || _expenses.indexOf(expense) < 0) {
             // the expense doesn't exist
@@ -554,28 +636,28 @@ public class MainController {
         return _expenses.remove(expense);
     }
 
-
-
-
-
     /**
      * Return all the appointments for a particular customer
      * @param customer Needed customer for appointment
      */
     public void getAppointmentsForCustomer(Customer customer, OnGetDataListener onGetDataListener) {
-
         _firebaseManager.getAppointmentsForCustomer(customer, onGetDataListener);
     }
 
     /**
-     * Return the first customer that matches the id.
-     * Return null if nothing was found
+     * searches for customer by id
+     * @param id search parameter
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getCustomerById(String id, OnGetDataListener onGetDataListener) {
         _firebaseManager.getCustomerWithId(id, onGetDataListener);
     }
 
-
+    /**
+     * returns the customer from the database that matches the id
+     * @param id search parameter
+     * @return found customer or null if not found
+     */
     public Customer getCustomerById(String id) {
         for (Customer customer: _customers){
             if (customer.getId().equals(id)){
@@ -591,29 +673,35 @@ public class MainController {
      * Return the first customer that matches the name. Return null
      * if nothing was found
      * @param name: customer name
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getCustomerByName(String name, OnGetDataListener onGetDataListener) {
         _firebaseManager.getCustomerWithName(name, onGetDataListener);
     }
 
-
     /**
      * Return a list of goals that are due by a certain date
-     * @param date: The month we're looking at
+     * @param date The month we're looking at
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getGoalsByDueDate(LocalDate date, OnGetDataListener onGetDataListener) {
         _firebaseManager.getGoalsWithEndDateBetween(date, date, onGetDataListener);
     }
 
-
     /**
      * This will return a number of goals
      * @param numGoals number of goal we want
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getGoalsWithLimit(int numGoals, OnGetDataListener onGetDataListener){
         _firebaseManager.getGoalsWithLimit(numGoals, onGetDataListener);
     }
 
+    /**
+     * gets a customer from the database
+     * @param name search parameter
+     * @return found customer
+     */
     public Customer getCustomerWithName(String name){
 
         for (Customer customer: _customers){
@@ -629,8 +717,7 @@ public class MainController {
 
 
     /**
-     * Return a list of customers that were added in the last month. This method may be
-     * changed later to accept a parameter that specifies the duration to check for
+     * Return a list of customers that were added in the last month
      */
     public void getNewCustomers(OnGetDataListener onGetDataListener) {
         LocalDate today = LocalDate.now();
@@ -657,14 +744,14 @@ public class MainController {
         return serviceMap;
     }
 
+    //todo we need this function???
     public List<Service> getAvailableServicesList() {
         return _services;
     }
 
-
     /**
      * Return a list of all the sales in the data collection.
-     * @return LinkedList<Sale>
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getAllSales(OnGetDataListener onGetDataListener) {
         _firebaseManager.getAllSales(onGetDataListener);
@@ -674,7 +761,7 @@ public class MainController {
     /**
      * Return the number of customers specified in the parameter
      * @param numOfCustomers the number of customers to be returned if available
-     * @return List of customers
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getFirstNumberCustomers(int numOfCustomers, OnGetDataListener onGetDataListener){
         _firebaseManager.getCustomersWithLimit(numOfCustomers, onGetDataListener);
@@ -684,6 +771,7 @@ public class MainController {
     /**
      * Return the number of sales specified in the parameter
      * @param numOfSales the number of sales to be returned if available
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getFirstNumberSales(int numOfSales, OnGetDataListener onGetDataListener){
         _firebaseManager.getSalesWithLimit(numOfSales, onGetDataListener);
@@ -692,6 +780,7 @@ public class MainController {
     /**
      * Return the number of appointments specified in the parameter
      * @param numAppointments the number of appointments to be returned if available
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getFirstNumberAppointments(int numAppointments, OnGetDataListener onGetDataListener){
         _firebaseManager.getAppointmentWithLimit(numAppointments, onGetDataListener);
@@ -703,6 +792,7 @@ public class MainController {
      *
      * This will return the most recent goals.
      * @param numGoals the number of goals to be returned if available
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getFirstNumberGoals(int numGoals, OnGetDataListener onGetDataListener){
         _firebaseManager.getGoalsWithLimit(numGoals, onGetDataListener);
@@ -714,27 +804,46 @@ public class MainController {
      *
      * This will return the most recent expenses.
      * @param numExpenses the number of expenses to be returned if available
+     * @param onGetDataListener listener because there is a delay on the reply
      */
     public void getFirstNumberExpenses(int numExpenses, OnGetDataListener onGetDataListener){
         _firebaseManager.getExpensesWithLimit(numExpenses, onGetDataListener);
     }
 
-
-    /*getters for financial report*/
+    /**
+     * getters for financial report
+     * @param beginDate begining of time span
+     * @param endDate end of time span
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getAppointmentsBetween(LocalDate beginDate, LocalDate endDate, OnGetDataListener onGetDataListener) {
          _firebaseManager.getAppointmentsBetween(beginDate, endDate, onGetDataListener);
     }
 
-    /*Getting Sales between a certain time*/
+    /**
+     * Getting Sales between a certain time
+     * @param beginDate beginning of time span
+     * @param endDate end of time span
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getSalesBetween(LocalDate beginDate, LocalDate endDate, OnGetDataListener onGetDataListener) {
         _firebaseManager.getSalesBetweenDates(beginDate, endDate, onGetDataListener);
     }
 
-    /*Getting Expenses between a certain time*/
+    /**
+     * Getting Expenses between a certain time
+     * @param beginDate beginning of the time span
+     * @param endDate end of the time span
+     * @param onGetDataListener listener because there is a delay on the reply
+     */
     public void getExpensesBetween(LocalDate beginDate, LocalDate endDate, OnGetDataListener onGetDataListener) {
         _firebaseManager.getExpensesBetweenDates(beginDate, endDate, onGetDataListener);
     }
 
+    /**
+     * gets the id for the newest customer
+     * @return id
+     */
     public String getIdForNewCustomer(){
         return _firebaseManager.getKeyForNewCustomer();
     }
